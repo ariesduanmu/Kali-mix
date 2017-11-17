2525ab4677a8019e21f79a7d5dbe723ea0081bfc
ludisposed <pjgvanschaik@gmail.com>
2017-11-17 16:44:04 -0600

    update frequency

--- Krypton/frequency.pl
+++ Krypton/frequency.pl
@@ -8,36 +8,15 @@ use feature 'say';
 use File::Slurp;
 
 
-my @filenames = ("found1", "found2", "found3");
+my @FILENAMES = ("found1", "found2", "found3");
 my @LETTERS = ("A" .. "Z");
-my %ENGLISH_FREQ = (
-    "E" => 12.702,
-    "T" => 9.056,
-    "A" => 8.167,
-    "O" => 7.507,
-    "I" => 6.966,
-    "N" => 6.749,
-    "S" => 6.327,
-    "H" => 6.094,
-    "R" => 5.987,
-    "D" => 4.253,
-    "L" => 4.025,
-    "C" => 2.782,
-    "U" => 2.758,
-    "M" => 2.406,
-    "W" => 2.360,
-    "F" => 2.228,
-    "G" => 2.015,
-    "Y" => 1.974,
-    "P" => 1.929,
-    "B" => 1.492,
-    "V" => 0.978,
-    "K" => 0.772,
-    "J" => 0.153,
-    "X" => 0.150,
-    "Q" => 0.095,
-    "Z" => 0.074
-);
+my @ENGLISH_FREQ = ("E", "T", "A", "O", "I", "N", "S", 
+                    "H", "R", "D", "L", "C", "U", "M", 
+                    "W", "F", "G", "Y", "P", "B", "V", 
+                    "K", "J", "X", "Q", "Z");
+
+my $ENCRYPTED_STRING = read_file("krypton4");
+$ENCRYPTED_STRING =~ s/\s+//g;
 
 
 sub count_frequency {
@@ -56,20 +35,28 @@ sub count_occurence {
 }
 
 sub decrypt {
-    my (%frequency) = @_;
-    foreach my $name ( sort { $frequency{$b} <=> $frequency{$a} } keys %frequency ) {
-        say "$name , $frequency{$name}";
+    my (%text_freq) = @_;
+    my %key;
+    my @text = ();
+
+    foreach my $name ( sort { $text_freq{$b} <=> $text_freq{$a} } keys %text_freq ) {
+        push @text, $name;
     }
 
-    # zip over the hashes?
+    # What is wrong here?
+    # I start to like perl :)
+    @key{@text} = @ENGLISH_FREQ;
+
+    foreach (split //, $ENCRYPTED_STRING) {
+        print "$key{$_}";
+    }
+    say "";
 }
 
-foreach (@filenames) {
+foreach (@FILENAMES) {
     my $text = read_file($_);
     $text =~ s/\s+//g;
-    my %file_frequency = count_frequency($text);
-    decrypt (%file_frequency);
-    say "-" x 20;
+    decrypt(count_frequency($text));
 }
 
 
