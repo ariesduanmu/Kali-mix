ac4f947ed73c1a4bb7e1f7e1f646fc5b7a0c506d
ariesduanmu <aries.duanmu@gmail.com>
2017-12-20 19:15:49 +0800

    dbd

--- Natas/db.pl
+++ Natas/db.pl
@@ -2,16 +2,18 @@ use strict;
 use warnings;
 use DBI;
 
-my $dbh = DBI->connect("DBI:mysql:database=test;host=localhost",
-                       "","",
+
+my $dbh = DBI->connect("DBI:mysql:database=MooCow;host=localhost",
+                       "root","toor",
                        {'RaiseError' => 1});
-$dbh->do("CREATE TABLE foo (username VARCHAR(100), password VARCHAR(100))");
-$dbh->do("INSERT INTO foo VALUES (natas31, ".$dbh->quote("natas31").")");
+eval{ $dbh->do("Drop Table foo") };
+$dbh->do("CREATE TABLE foo (name VARCHAR(100), password VARCHAR(20))");
+$dbh->do("INSERT INTO foo VALUES(".$dbh->quote("natas30").", ".$dbh->quote("password").")");
 
 my $sth = $dbh->prepare("SELECT * FROM foo");
 $sth->execute();
 while(my $ref = $sth->fetchrow_hashref()) {
-    print "Found a row: id = $ref->{'username'}, name = $ref->{'password'}\n";
+    print "Found a row: name = $ref->{'name'}, password = $ref->{'password'}\n";
 }
 $sth->finish();
 $dbh->disconnect();
