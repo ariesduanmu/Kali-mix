29d8f0e685d953273e098900967408ea92b34d94
ariesduanmu <aries.duanmu@gmail.com>
2018-07-31 21:57:06 +0800

    add thread

--- dirb.py
+++ dirb.py
@@ -10,11 +10,10 @@ import argparse
 import requests
 from urllib.parse import urljoin
 
-NUMBER_OF_THREADS = 8
 queue = Queue()
 
-def create_workers(response_codes):
-    for _ in range(NUMBER_OF_THREADS):
+def create_workers(response_codes, number_of_threads):
+    for _ in range(number_of_threads):
         t = threading.Thread(target=work, args=(response_codes,))
         t.daemon = True
         t.start()
@@ -51,13 +50,14 @@ def print_response_code(url, response_codes):
 def parse_arguments():
     """Arguments parser."""
     parser = argparse.ArgumentParser(usage='%(prog)s [options] <url>',
-                                     description='Fun with xor by @Ludisposed',
+                                     description='Fun with xor by @Ludisposed @Qin',
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      epilog=dedent('''Examples:
-                                                      python dirbuster.py  -c "200, 301,        401" -w "wordlist.txt" -e "html,php,py" https://codereview.stackexchange.com:443/questions'''))
+                                                      python dirbuster.py  -c "200, 301,        401" -w "wordlist.txt" -e "html,php,py" <url>'''))
     parser.add_argument('-c', '--code', type=str, help='Http response codes to filter on')
     parser.add_argument('-w', '--wordlist', type=str, help='Wordlist you want to fuzz with')
     parser.add_argument('-e', '--extension', type=str, help='Filename extensions you want to fuzz')
+    parser.add_argument('-t', '--thread', type=int, default=10, help="number of thread")
     parser.add_argument('url', type=str, help='Url you want to fuzz')
     args = parser.parse_args()
 
@@ -85,11 +85,11 @@ def parse_arguments():
     if not args.extension is None:
         args.extension = re.sub(r"\s+", "", args.extension).split(",")
 
-    return args.url, args.code, args.wordlist, args.extension
+    return args.url, args.code, args.wordlist, args.extension, args.thread
 
 if __name__ == '__main__':
-    url, response_codes, wordlist, file_extensions = parse_arguments()
-    create_workers(response_codes)
+    url, response_codes, wordlist, file_extensions, number_of_threads = parse_arguments()
+    create_workers(response_codes, number_of_threads)
     dir_buster(url, wordlist, file_extensions)
 
 
