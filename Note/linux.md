98a7b215cb61689a7042d1832118af6003ac9b94
ariesduanmu <aries.duanmu@gmail.com>
2018-08-02 22:39:26 +0800

    add random pass

---
+++ Note/linux.md
@@ -0,0 +1,16 @@
+##Create User
+* adduser aries
+
+##Change password
+* passwd
+
+##Delete User
+* deluser aries
+
+##Install Openvpn
+* apt-get update && apt-get upgrade && apt-get install openvpn unzip
+
+##Install Metasploit
+* apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev libyaml-dev curl zlib1g-dev
+
+* apt install build-essential curl nodejs
---
+++ randompass.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#!/bin/usr/env python
+
+import sys
+import argparse
+from textwrap import dedent
+from random import choice
+from string import ascii_lowercase, ascii_uppercase, digits
+
+def random_password(case, length):
+    options = [ascii_lowercase, ascii_uppercase, digits, "!@#$%"]
+    case = "{:04b}".format(case)
+    letters = "".join(options[i] for i in range(4) if case[i] == "1")
+    print(f'[+] Password: {"".join([choice(letters) for _ in range(length)])}')
+
+def parse_arguments():
+    """Arguments parser."""
+    parser = argparse.ArgumentParser(usage='%(prog)s  [opt]',
+                                     description='create random password',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=dedent('''
+                                                      Model 01: !@#$%
+                                                      Model 02: 0-9
+                                                      Model 03: 0-9 + !@#$%
+                                                      Model 04: A-Z
+                                                      Model 05: A-Z + !@#$%
+                                                      Model 06: A-Z + 0-9
+                                                      Model 07: A-Z + 0-9 + !@#$%
+                                                      Model 08: a-z 
+                                                      Model 09: a-z + !@#$%
+                                                      Model 10: a-z + 0-9
+                                                      Model 11: a-z + 0-9 + !@#$%
+                                                      Model 12: a-z + A-Z
+                                                      Model 13: a-z + A-Z + !@#$%
+                                                      Model 14: a-z + A-Z + 0-9
+                                                      Model 15: a-z + A-Z + 0-9 + !@#$%"
+
+                                                      Examples:
+                                                      python randompass.py  -m 1 -l 10 '''))
+    parser.add_argument('-m', '--model', type=int, default=15, help='Model between 1 and 15, default 15')
+    parser.add_argument('-l', '--length', type=int, default=8, help='password length, default 8')
+    args = parser.parse_args()
+
+    if args.model is not None and (args.model <= 0 or args.model > 15):
+        print("[!] Please chose model between 1 and 15")
+        sys.exit(1)
+
+    return args.model, args.length
+
+if __name__ == '__main__':
+    model, length = parse_arguments()
+    random_password(model, length)
+
